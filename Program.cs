6e3703cdfe24b5c7b7d867e58b5f243f00ac5318
demisoncraze <54923119+demisoncraze@users.noreply.github.com>
2019-09-05 11:28:03 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5 * 6;
+            int b = 6 * 5;
+            Console.WriteLine("HEY!");
+            if (a > b)
+                Console.WriteLine("A=" + a);
+            else
+                Console.WriteLine("Lets try again");
+            Console.ReadKey();
+            
+               
+
+
+            
+        }
+    }
+}
